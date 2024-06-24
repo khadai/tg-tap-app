c924c8cb0a83de1cf849922efc1d39519dc5443d
khadai <mykhailo.khadai@gmail.com>
2024-06-24 21:18:37 +0300

    fixed config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  // server: {
-  //   port: 443,
-  //   host: "0.0.0.0",
-  //   hmr: {
-  //     host: 'tg-mini-app.local',
-  //     port: 443,
-  //   },
-  //   https: {
-  //     key: './.cert/localhost-key.pem',
-  //     cert: './.cert/localhost.pem',
-  //   },
-  // },
+    plugins: [react(), svgr()],
+    base: '/tg-tap-app/'
+    // server: {
+    //   port: 443,
+    //   host: "0.0.0.0",
+    //   hmr: {
+    //     host: 'tg-mini-app.local',
+    //     port: 443,
+    //   },
+    //   https: {
+    //     key: './.cert/localhost-key.pem',
+    //     cert: './.cert/localhost.pem',
+    //   },
+    // },
 })
